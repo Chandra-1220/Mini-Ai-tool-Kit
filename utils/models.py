d47e6fcd66787b51bf3483072b68cd321d8991b7
Chandra-1220 <chandraharsha1220@gmail.com>
2026-07-17 18:05:00 +0530

    Create models.py

---
+++ utils/models.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from transformers import pipeline
+
+
+# -------------------------------
+# Sentiment Analysis
+# -------------------------------
+@st.cache_resource
+def load_sentiment_model():
+    return pipeline(
+        "sentiment-analysis",
+        model="distilbert-base-uncased-finetuned-sst-2-english"
+    )
+
+
+# -------------------------------
+# Text Summarization
+# -------------------------------
+@st.cache_resource
+def load_summarizer():
+    return pipeline(
+        "summarization",
+        model="sshleifer/distilbart-cnn-12-6"
+    )
+
+
+# -------------------------------
+# Question Answering
+# -------------------------------
+@st.cache_resource
+def load_qa_model():
+    return pipeline(
+        "question-answering",
+        model="distilbert-base-cased-distilled-squad"
+    )
+
+
+# -------------------------------
+# Translation (English → French)
+# -------------------------------
+@st.cache_resource
+def load_translator():
+    return pipeline(
+        "translation",
+        model="facebook/nllb-200-distilled-600M",
+        
+    )
+
+
+# -------------------------------
+# Named Entity Recognition
+# -------------------------------
+@st.cache_resource
+def load_ner_model():
+    return pipeline(
+        "ner",
+        model="dslim/bert-base-NER",
+        aggregation_strategy="simple"
+    )
+
+
+# -------------------------------
+# Zero-Shot Classification
+# -------------------------------
+@st.cache_resource
+def load_zero_shot():
+    return pipeline(
+        "zero-shot-classification",
+        model="facebook/bart-large-mnli"
+    )
